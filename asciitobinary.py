3ee073c5cfff2ea12a3cf7056ea378f566865152
matheusoliveira23 <matheuscavalcante5b@gmail.com>
2014-10-09 14:21:23 -0300

    Create asciitobinary.py

---
+++ asciitobinary.py
@@ -0,0 +1,5 @@
+#este programa converte um caractere para seu binario equivalente seguindo a tabela ascii
+texto=input("digite alguma coisa: ")
+a=int(ord(texto))
+b=bin(a)
+print("o binario da lera digitada é",b)
