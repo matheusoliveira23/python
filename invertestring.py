712388aabd392d6461b72fe9c2f97c5acdd67ed5
matheus cavalcante de oliveira <matheuscavalcante5b@gmail.com>
2014-10-16 15:07:47 -0300

    correção de bugs em inverte string

--- invertestring.py
+++ invertestring.py
@@ -1,11 +1,9 @@
 #inverte nome é inutil mas deu vontade de fazer
 texto=''
-texto=input(texto)
-tam=len(texto)
-i=0
-inv=slice(texto)
-for i in range(tam):
-    inv[i]=inv[i-1]
+texto=input('digite uma palavra: ')
+inv=''
+inv=texto[::-1]
+print('a palavra invertida é: ',inv)
     
 
 
