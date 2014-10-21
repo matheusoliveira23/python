3fe533aeb94bd9ced0eba563c736fc30c38a0e7a
matheusoliveira23 <matheuscavalcante5b@gmail.com>
2014-10-21 00:28:48 -0300

    Update asciitobinarywithtext.py

--- asciitobinarywithtext.py
+++ asciitobinarywithtext.py
@@ -1,5 +1,9 @@
 #programa converte uma string inteira para binario
-from struct import *
-
-pack("cash", 1, 2, 3, 4)
+string=input('digite uma palavra ')
+lista=list(string)
+binario=[]
+i=0
+for i in range(len(lista)):
+  binario.append(bin(int(ord(lista[i])))
+print(binario)
 
