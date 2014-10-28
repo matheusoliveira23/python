035e68d94f2977e3815ee64b3f3d87fbdf3ce687
matheus cavalcante de oliveira <matheuscavalcante5b@gmail.com>
2014-10-28 13:36:17 -0300

    update

--- vaiquecrypto.py
+++ vaiquecrypto.py
@@ -4,8 +4,11 @@ textoplano=input('digite a mensagem: ')
 vet=list(textoplano)
 random.shuffle(vet)
 num=[]
+b=[]
 for i in range(len(vet)):
     num.append(ord(vet[i]))
     num.append((num[i]+40))
     random.shuffle(num)
+    b.append(bin(num[i]))
 print('mensagem encriptada na forma decimal: ',num)
+print('mensagem encriptada na forma binaria: ',b)
