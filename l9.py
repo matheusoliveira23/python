cda017a05d7d5ed6272a4b90548658ef06ab580e
matheusoliveira23 <matheuscavalcante5b@gmail.com>
2015-03-25 15:54:55 -0300

    Create l9.py

---
+++ l9.py
@@ -0,0 +1,4 @@
+kmrodado=float(input("digite a quantidade de km rodado\t"))
+qtdias=int(input("digite a quantidade de dias \tque o cliente \tpassou com o carro \t))
+preco=(qtdias*60)+(kmrodado*0,15)
+print(preco)
