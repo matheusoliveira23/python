2522c6f407d2016771bd6fba4804b1c77ca7dab6
matheusoliveira23 <matheuscavalcante5b@gmail.com>
2015-03-25 16:41:54 -0300

    Create l10pythonparazumbis.py
    incompleto

---
+++ l10pythonparazumbis.py
@@ -0,0 +1,4 @@
+qtcigarrosdia=int(input("quantos cigarros voce fuma por dia ? \t"))
+anosquefumou=int(input("à quantos anos voce fuma? \t"))
+diasamenosdevida=(anosquefumou/365)-(*qtcigarrosdia)
+print(diasamenosdevida)
